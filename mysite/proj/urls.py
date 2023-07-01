6a7facc040034da6a167615833898cf866a2082c
Lucas Nithael <lucasnithael0101@gmail.com>
2023-07-01 17:40:35 -0300

    refactor: ajustes na url para adequação a view  #30 #31

--- mysite/proj/urls.py
+++ mysite/proj/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from . import views
+from .views import ProjectView, homeProject
 
 app_name = 'proj'
 
 urlpatterns = [
-    path('', views.home_project, name='home'),
-    path('project/detail/<int:project_id>', views.detail_project, name='detail'),
-    path('project/register/', views.register_project, name='register'),
-    path('project/create/', views.create_project, name='create'),
+    path('', homeProject, name='home'),
+    path('project/detail/<int:project_id>', ProjectView.detailProject, name='detail'),
+    path('project/register/', ProjectView.registerProject, name='register'),
+    path('project/create/', ProjectView.saveProject, name='create'),
 
 
 ]
