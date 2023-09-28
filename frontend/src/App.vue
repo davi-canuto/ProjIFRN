45dd735c0385f657444235b7a7ac6ecf8d88a1f8
dlavinia <dlavinia2003@gmail.com>
2023-09-28 20:32:49 -0300

    Seção Explore da home

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,51 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import ExploreCard from './components/cards/ExploreCard.vue';
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section >
+    <h1 class="title-section"> Explore nossa coleção de projetos </h1>
+    <div class="cards">
+      <ExploreCard :data=22 title="Em Desenvolvimento" 
+      description="Testemunhe a evolução dos nossos projetos promissores em pleno desenvolvimento." 
+      title_gradient="purple" />
+
+      <ExploreCard :data=35 title="Projetos Finalizados" 
+      description="Explore nossa galeria de projetos finalizados, resultados brilhantes e conquistas concluídas." 
+      title_gradient="green" />
+
+      <ExploreCard :data=10 title="Projetos em Espera" 
+      description="Explore nossos projetos em espera, prontos para serem retomados e levados adiante." 
+      title_gradient="red" />
+    </div>
+   
+
+  </section>
+  
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  section{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }  
+  .cards{
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+  }
+  .title-section{
+    color: #131740;
+    font-size: 44px;
+    font-weight: 700;
+    line-height: 66px; 
+    text-align: center;
+    margin-bottom: 4rem;
+  }
 </style>
+
+
---
+++ frontend/src/components/cards/ExploreCard.vue
@@ -0,0 +1,70 @@
+<script setup>
+defineProps({
+    data: Number,
+    title: String,
+    description: String,
+    title_gradient: String 
+})
+</script>
+
+<template>
+    <article >
+        <h1 :class=title_gradient>{{data}}</h1>
+        <h2>{{title}}</h2>
+        <p>{{description}}</p>
+    </article>
+</template>
+
+
+
+<style scoped>
+article {
+  display: flex;
+  width: 30%;
+  padding: 50px 28px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center; 
+
+  border-radius: 20px;
+  background-color: #FFF;
+  box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.05);
+
+  text-align: center;
+  font-style: normal;
+}
+h1{
+  font-size: 80px;
+  font-weight: 600;
+  line-height: 91px; 
+
+  color: black;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+h2{
+  color: #1B1B1B;
+  font-size: 26px;
+  font-weight: 600;
+  line-height: 54px; 
+}
+
+p{
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 21px; 
+  color: #4A5568; 
+}
+
+.purple  {
+  background-image: linear-gradient(93deg, #FE6AB7 3.55%, #9B4AEB 47.23%, #3931F9 97.46%);
+}
+.green{
+  background-image: linear-gradient(93deg, #2AF598 -29.31%, #08AEEA 106.47%); 
+}
+.red{
+  background-image: linear-gradient(93deg, #FF512F 3.55%, #DD2476 97.46%); 
+}
+</style>
